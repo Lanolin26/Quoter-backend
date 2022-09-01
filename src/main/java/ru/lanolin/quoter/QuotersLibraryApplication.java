b25ead3254d7816cec3d2293cdd0ab08f898f784
Artem Kiselev <lanolin652@gmail.com>
2022-09-01 15:19:16 +0300

    init

---
+++ src/main/java/ru/lanolin/quoter/QuotersLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.lanolin.quoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotersLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotersLibraryApplication.class, args);
+	}
+
+}
