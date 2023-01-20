513d3c88c10a7dda0ad2b7ce5ef40dd27ca6ce5b
Artem Kiselev <lanolin652@gmail.com>
2023-01-20 23:09:35 +0300

    update main component

--- src/main/java/ru/lanolin/quoter/backend/config/MvcConfig.java
+++ src/main/java/ru/lanolin/quoter/backend/config/MvcConfig.java
@@ -1,16 +1,29 @@
 package ru.lanolin.quoter.backend.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/js/**")
-				.addResourceLocations("classpath:/static/js/");
-	}
+
+    private static final Map<String, String> resourceAndPath = new HashMap<>();
+
+    static {
+        resourceAndPath.put("/js/**", "classpath:/static/js/");
+        resourceAndPath.put("/css/**", "classpath:/static/css/");
+        resourceAndPath.put("/fonts/**", "classpath:/static/fonts/");
+        resourceAndPath.put("/favicon.ico", "classpath:/static/favicon.ico");
+    }
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        resourceAndPath.forEach((k, v) -> registry.addResourceHandler(k).addResourceLocations(v));
+    }
 }
