829a6ab57fb55002fef08ec183c194ae6a0be4a0
Artem Kiselev <lanolin652@gmail.com>
2023-01-20 23:12:48 +0300

    add sqlite support

---
+++ src/main/resources/db/migration/sqlite/V1__drop.sql
@@ -0,0 +1,5 @@
+drop table if exists quote_entity;
+drop table if exists quote_from_entity;
+drop table if exists quote_from_type_entity;
+drop table if exists user_entity;
+drop table if exists user_entity_roles;
---
+++ src/main/resources/db/migration/sqlite/V2__init.sql
@@ -0,0 +1,53 @@
+-- Создание
+
+create table user_entity
+(
+    id       integer not null
+        constraint user_entity_pk
+            primary key autoincrement,
+    login    text    not null constraint user_entity_pk2 unique,
+    name     text    not null,
+    password text    not null,
+    img      blob
+);
+
+create table user_entity_roles
+(
+    user_entity_id integer not null
+        constraint user_entity_roles_user_entity_id_fk
+            references user_entity,
+    roles          text    not null
+);
+
+create table quote_source_type_entity
+(
+    id   integer not null
+        constraint quote_source_type_entity_pk
+            primary key autoincrement,
+    type text default not null
+);
+
+create table quote_source_entity
+(
+    id          integer not null
+        constraint quote_source_entity_pk
+            primary key autoincrement,
+    source_name text    not null,
+    type_id     integer not null
+        constraint quote_source_entity_quote_source_type_entity_id_fk
+            references quote_source_type_entity
+);
+
+create table quote_entity
+(
+    id        integer not null
+        constraint quote_entity_pk
+            primary key autoincrement,
+    "text"      text    not null,
+    author_id integer not null
+        constraint quote_entity_user_entity_id_fk
+            references user_entity,
+    source_id integer not null
+        constraint quote_entity_quote_source_entity_id_fk
+            references quote_source_entity
+);
